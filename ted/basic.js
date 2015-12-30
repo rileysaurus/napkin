d796b28f9038f389d2beff19e014eabf756c1b3e
Benjamin Cauldwell <benjamin.cauldwell@gmail.com>
2015-12-29 17:07:53 -0800

    testing

--- ted/basic.js
+++ ted/basic.js
@@ -9,9 +9,9 @@ ted.describe('basic', function(info) {
     it('should work 1', function() {
       return foo();
     });
-    it('should work 2', function() {
+    /*it('should work 2', function() {
       return foo();
-    });
+    });*/
   });
 
 });
